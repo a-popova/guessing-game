43c9b0f7756d2b9a8021a103c53feb188c0f8bf7
a-popova <papova.anastasiya.13@gmail.com>
2019-10-08 15:11:15 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        return Math.round((this.left + this.right) / 2);
     }
 
     lower() {
-
+        this.right = Math.round((this.left + this.right) / 2);
     }
 
     greater() {
-
+        this.left = Math.round((this.left + this.right) / 2);
     }
-}
+  }
 
 module.exports = GuessingGame;
